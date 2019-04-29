fbdb1451c6048905b984413d67a2a0306ce9998d
nikolajgriskin <din.nick@bk.ru>
2019-04-29 16:47:07 +0500

    A simple chat app initialized

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello!');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/html/index.html');
+});
+
+io.on('connection', function (socket) {
+  console.log('an user connected');
+
+  socket.broadcast.emit('hi');
+
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
